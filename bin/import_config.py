cd07851d77679cdac95e08b1d977ecd4b26c3161
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-02-21 16:11:14 +0100

    0.1 wow it works

---
+++ bin/import_config.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+
+def inject_all_configs(config_data):
+    'Given a hash from YAML it creates one gcloud config per key'
+    # injects config as per tree.
+    for config_name, properties in config_data['configurations'].items():
+        # Create configuration if it doesn't exist
+        os.system(f"gcloud config configurations create {config_name}")
+
+        for property_name, value in properties.items():
+            os.system(f"gcloud config set {property_name} {value} --configuration {config_name}")
+
+def main():
+    #with open('.gcloudconfig.yaml', 'r') as file:
+    with open('.gcloudconfig.yaml', 'r') as file:
+        config_data = yaml.safe_load(file)
+
+    inject_all_configs(config_data)
+
+    # for k, val in config_data['local_config'].items():
+    #     print(f"{k} -> {val}")
+    if config_data['local_config']['auto'] == True:
+        full_dir =  os.getcwd()
+        local_dir = os.path.basename(full_dir)
+        # Alternative:
+        # last_part = current_dir.rsplit('/', 1)[1]
+        print(f'Setting config with same name as local dir: {local_dir}')
+        os.system(f"gcloud config configurations activate {local_dir}")
+    else:
+        print(f"local_config[auto] = '{ config_data['local_config']['auto'] }'")
+
+    # Final
+
+    os.system(f"gcloud config list")
+
+main()
