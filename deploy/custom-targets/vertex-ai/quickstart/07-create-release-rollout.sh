b5b97f93ed49693a68e4c89e85ccbeff9f7faae2
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-11 16:01:34 +0100

    fixing template

--- deploy/custom-targets/vertex-ai/quickstart/07-create-release-rollout.sh
+++ deploy/custom-targets/vertex-ai/quickstart/07-create-release-rollout.sh
@@ -5,14 +5,14 @@
 
 set -euo pipefail
 
-#
+# 11mar rel04: fixed other stuff in configuration/
 # 11mar rel04: moved from projects/rick-and-nardy-demo/locations/us-central1/endpoints/quickstart-prod to projects/rick-and-nardy-demo/locations/us-central1/endpoints/quickstart-dev
 # 11mar rel03: pushed to dev 1st time
 # 11mar -----  ricc creates dev target before prod. Super weird mutation: https://screenshot.googleplex.com/8L2yiyDY8aQ8Qpb
 # 22feb rel02: same -> prod
 # 22feb rel01: first one -> prod
 
-gcloud deploy releases create release-004 \
+gcloud deploy releases create release-005 \
     --delivery-pipeline=vertex-ai-cloud-deploy-pipeline \
     --project=$PROJECT_ID \
     --region=$REGION \
---
+++ deploy/custom-targets/vertex-ai/quickstart/configuration/README.md
@@ -0,0 +1,15 @@
+
+Ciao Riccardo,
+this is just to remind you that this dir gets copied in its entirety (with some hydration)
+
+* FROM: deploy/custom-targets/vertex-ai/quickstart/configuration/ (TEMPLATES)
+* TO: deploy/custom-targets/vertex-ai/quickstart/out/configuration/ (HYDRATED version)
+
+This means: edit the latter at your own risk.
+
+Let me also add
+
+
+## VERSION HISTORY
+
+* 11mar24: Added dev/ folder too to comply with Ivan requirements.
