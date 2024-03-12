a59fb611e96ad7d944e59eb9780f3b8a3d455c60
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-12 14:18:06 +0100

    stil nothing

--- deploy/custom-targets/vertex-ai/quickstart/67-create-ivan-release.sh
+++ deploy/custom-targets/vertex-ai/quickstart/67-create-ivan-release.sh
@@ -15,9 +15,23 @@ mkdir -p "${TMPDIR_IVAN}"
 
 echo "0. Vars:"
 echo "+ CD_DEPLOYABLE_MODEL: $CD_DEPLOYABLE_MODEL"
+echo "+ TMPDIR_IVAN: $TMPDIR_IVAN"
+echo "+ PROD_DEMO_ENDPOINT_ID: $PROD_DEMO_ENDPOINT_ID"
+echo "+ DEV_DEMO_ENDPOINT_ID: $DEV_DEMO_ENDPOINT_ID"
 
 echo "1. Replacing vars in this dir: $TMPDIR_IVAN"
-./replace_variables.sh -p $PROJECT_ID -r $REGION -e $ENDPOINT_ID -t $TMPDIR_IVAN -d $DEV_ENDPOINT_ID
+
+# Normal test version
+#./replace_variables.sh -p $PROJECT_ID -r $REGION -e $ENDPOINT_ID -t $TMPDIR_IVAN -d $DEV_ENDPOINT_ID
+# Demo version
+#PROD_DEMO_ENDPOINT_ID="demo-prod"
+#DEV_DEMO_ENDPOINT_ID="demo-dev"
+# demo-dev     dev-endpoint
+#demo-prod    prod-endpoint
+#PROD_DEMO_ENDPOINT_NAME="prod-endpoint"
+#DEV_DEMO_ENDPOINT_NAME="dev-endpoint"
+#./replace_variables.sh -p $PROJECT_ID -r $REGION -e $PROD_DEMO_ENDPOINT_NAME -t $TMPDIR_IVAN -d $DEV_DEMO_ENDPOINT_NAME
+./replace_variables.sh -p $PROJECT_ID -r $REGION -e $PROD_DEMO_ENDPOINT_ID -t $TMPDIR_IVAN -d $DEV_DEMO_ENDPOINT_ID
 
 echo 2. Lets now apply CD config..
 gcloud deploy apply --file=$TMPDIR_IVAN/clouddeploy.yaml --project=$PROJECT_ID --region=$REGION
@@ -28,8 +42,12 @@ gcloud deploy apply --file=$TMPDIR_IVAN/clouddeploy.yaml --project=$PROJECT_ID -
 
 #############################################
 # Release name
-REL_NAME_BASE="017"
-# crated demo-dev e demo-prod
+REL_NAME_BASE="020"
+# v20 new naming. stil nada.
+# v19 I have a feeling i never sent the 17 but sent the 16 twice. So there you go, trying again 19 with IDS.
+# v18 using name instead
+# v17 ->
+# 12mar v016 created demo-dev e demo-prod and using those. if it doesnt work using the other names.
 # 12mar v016 after lunch. maybe it helps. Also Ivan pinned a version.
 # 12mar v015 added CD_DEPLOYABLE_MODELto gaic and piggybacked in all scripts
 # 12mar v014 direnv allow
@@ -49,3 +67,4 @@ gcloud deploy releases create "$REL_NAME" \
     --deploy-parameters="customTarget/vertexAIModel=projects/$PROJECT_ID/locations/$REGION/models/$CD_DEPLOYABLE_MODEL"
 
 echo "😍 All good."
+echo "🚀 Deployed release '$REL_NAME'.. to model '$CD_DEPLOYABLE_MODEL'.."
--- deploy/custom-targets/vertex-ai/quickstart/74-create-california-vertexai-ep.sh
+++ deploy/custom-targets/vertex-ai/quickstart/74-create-california-vertexai-ep.sh
@@ -7,16 +7,21 @@ set -euo pipefail
 # ENDPOINT_ID: "quickstart-prod"
 # DEV_ENDPOINT_ID: "2371068237996621824" # https://screenshot.googleplex.com/AbYAAZu2PYd9wTz
 
-PROD_DEMO_ENDPOINT_ID="demo-prod"
- DEV_DEMO_ENDPOINT_ID="demo-dev"
+#PROD_DEMO_ENDPOINT_ID="demo-prod"
+#DEV_DEMO_ENDPOINT_ID="demo-dev"
 
+# # PROD
+# gcloud ai endpoints create --display-name prod-endpoint --endpoint-id "$PROD_DEMO_ENDPOINT_ID" --region "$REGION" --project "$PROJECT_ID"
+# # DEV
+# gcloud ai endpoints create --display-name dev-endpoint --endpoint-id "$DEV_DEMO_ENDPOINT_ID" --region "$REGION" --project "$PROJECT_ID"
 # PROD
-gcloud ai endpoints create --display-name prod-endpoint --endpoint-id "$PROD_DEMO_ENDPOINT_ID" --region "$REGION" --project "$PROJECT_ID"
-
+gcloud ai endpoints create --display-name $PROD_DEMO_ENDPOINT_ID --endpoint-id "$PROD_DEMO_ENDPOINT_ID" --region "$REGION" --project "$PROJECT_ID"
 # DEV
-gcloud ai endpoints create --display-name dev-endpoint --endpoint-id "$DEV_DEMO_ENDPOINT_ID" --region "$REGION" --project "$PROJECT_ID"
+gcloud ai endpoints create --display-name $DEV_DEMO_ENDPOINT_ID  --endpoint-id "$DEV_DEMO_ENDPOINT_ID"  --region "$REGION" --project "$PROJECT_ID"
 
 # $ gcloud ai endpoints list
 # ENDPOINT_ID  DISPLAY_NAME
 # demo-dev     dev-endpoint
 # demo-prod    prod-endpoint
+
+gcloud ai endpoints list
