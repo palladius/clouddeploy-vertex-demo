c2dcb99cdf9067514f1c58879cfa936126fcf098
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-13 19:18:31 +0100

    3.1 nearly there

--- deploy/custom-targets/vertex-ai/quickstart/09-inspector.sh
+++ deploy/custom-targets/vertex-ai/quickstart/09-inspector.sh
@@ -9,7 +9,10 @@ set -euo pipefail
 
 echo '😎 Lets now see if aliases are assigned:'
 CD_DEPLOYABLE_MODEL_ID="3485927948584747008"
-gcloud ai models describe "$CD_DEPLOYABLE_MODEL_ID" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
+echodo gcloud ai models describe "$CD_DEPLOYABLE_MODEL_ID" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
+echodo gcloud ai models describe "$CD_DEPLOYABLE_MODEL_ID@1" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
+echodo gcloud ai models describe "$CD_DEPLOYABLE_MODEL_ID@2" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
+echodo gcloud ai models describe "$CD_DEPLOYABLE_MODEL_ID@3" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
 #echodo gcloud ai models describe "california_reg_model" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
 #gcloud ai models describe "$CD_DEPLOYABLE_MODEL" --region $REGION --project $PROJECT_ID --format "(versionAliases)"
 
