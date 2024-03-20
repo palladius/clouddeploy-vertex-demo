085532ba550ddc1db18feea6e2d36352980ef563
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-20 11:43:30 +0100

    ritocchino in the

--- deploy/verify-evaluate-cloud-metrics/06-create-release-XXX.sh
+++ deploy/verify-evaluate-cloud-metrics/06-create-release-XXX.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
+set -euo pipefail
+
 # needs skaffold.yaml
 RELNAME="${1:-XXX}"
+REL_DESC="Trying something new"
 
 cd cloud-deploy/ &&
-gcloud deploy releases create "rel-$RELNAME" \
+  gcloud deploy releases create "rel-$RELNAME" \
    --project="rick-and-nardy-demo" \
+   --description="[Verify] $REL_DESC" \
    --region=us-central1 \
    --delivery-pipeline='ricc-verify-metrics'  \
    --images=my-app-image=gcr.io/cloudrun/hello@sha256:98cdb98c2d97a67d5e9183beedfec98ca9d5967acd874409b4800bf4d1a51710
