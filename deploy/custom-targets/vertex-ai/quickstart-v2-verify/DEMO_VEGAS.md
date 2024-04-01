78b4b0e12977fe53187e9f86625aedf0960a325b
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-04-01 16:21:38 +0200

    3.3 better docs for Demo

--- deploy/custom-targets/vertex-ai/quickstart-v2-verify/DEMO_VEGAS.md
+++ deploy/custom-targets/vertex-ai/quickstart-v2-verify/DEMO_VEGAS.md
@@ -2,10 +2,54 @@
 
 Self: https://github.com/palladius/clouddeploy-vertex-demo/blob/main/deploy/custom-targets/vertex-ai/quickstart-v2-verify/DEMO_VEGAS.md
 
+## Recording Setup
+
+* Linux (derek): _ctrl-space_ + "Take a screenshot"  (screencast recorder)
+    * Goes into derek:~/Videos/Screencasts
+* Mac: `Camtasia`
+* Chromebook / Chrome: **internal** chromext ("Screencast").
+
+Tips🧮 :
+* Make sure you dont show the URL and the CTRL SHIFT B below it.
+* start with some things open:
+    1. Slides
+    2. PSA doc
+    3. CD - pipeline: https://pantheon.corp.google.com/deploy/delivery-pipelines/us-central1/vertex-demo-verify?e=-13802955&mods=logs_tg_staging&project=rick-and-nardy-demo
+    4. Model registry (model view): https://pantheon.corp.google.com/vertex-ai/models?e=-13802955&mods=logs_tg_staging&project=rick-and-nardy-demo
+    5. Online predictions (Endpoints view): https://pantheon.corp.google.com/vertex-ai/online-prediction/endpoints?project=rick-and-nardy-demo&e=-13802955&mods=logs_tg_staging
+    6. Price trix - emptied: https://docs.google.com/spreadsheets/d/17emmad7-UAUL298z-pYBWD76NWbFSmkphnpwSguZFTI/edit?resourcekey=0-ffn4eeakYSpU0QYRancZag#gid=2075780709
+
 ### Setup and auth (make sure you have access)
 
 * `gaic` - set up  env vars. `git diff` if it fails.
 * `./11-explore-models.sh` - check GCLOUD AUTH works.
+
+### Pre-deployment Cleanup
+
+1. remove ALIASES from all VERSIONS: https://pantheon.corp.google.com/vertex-ai/models/locations/us-central1/models/8413639997114023936?e=-13802955&mods=logs_tg_staging&project=rick-and-nardy-demo
+    1. 3 dots on v1 side.
+    2. 1. click `edit alias`
+    3. remove everything except v1 or v2
+    4. Same on v2 side (3 dots, ...)
+
+  https://pantheon.corp.google.com/vertex-ai/online-prediction/locations/us-central1/endpoints/demo24-dev?project=rick-and-nardy-demo
+
+  forse trovi meglio TODO
+
+1. Undeploy endpoints so that release actually DEPLOYS to them:
+    1. Undeploy from demo24-dev: https://pantheon.corp.google.com/vertex-ai/online-prediction/locations/us-central1/endpoints/demo24-dev?project=rick-and-nardy-demo
+    2. 3 dots -> "Undeploy model from endpoint"
+    3. Undeploy from demo24-prod: https://pantheon.corp.google.com/vertex-ai/online-prediction/locations/us-central1/endpoints/demo24-prod?project=rick-and-nardy-demo
+    4. 3 dots -> "Undeploy model from endpoint"
+    5. Same on demo24-preprod: https://pantheon.corp.google.com/vertex-ai/online-prediction/locations/us-central1/endpoints/demo24-preprod?e=-13802955&mods=logs_tg_staging&project=rick-and-nardy-demo
+    6. Verify that all endpoints are now empty: https://pantheon.corp.google.com/vertex-ai/online-prediction/endpoints?project=rick-and-nardy-demo&e=-13802955&mods=logs_tg_staging
+* Check ALL VERSIONS link: https://pantheon.corp.google.com/vertex-ai/models/locations/us-central1/models/8413639997114023936?project=rick-and-nardy-demo
+
+
+1. Make sure v1 is NOT deployed. You want to deploy a v1 first. For instance check :
+    2. https://pantheon.corp.google.com/deploy/delivery-pipelines/us-central1/vertex-demo-verify?e=-13802955&mods=logs_tg_staging&project=rick-and-nardy-demo that all 3 endpoints are on v2. https://screenshot.googleplex.com/tGrgcFiJfkpuHjt
+
+**IMPORTANT** Actually if you undeploy them it takes A LONG TIME! 5-6 minutes or more. So maybe worth NOT undeploying but make sure they deploy to v2.
 
 ### [OPTIONAL] Create Endpoints and check
 
@@ -17,6 +61,9 @@ Only run if you dont see the 3 endpoinst.
 
 
 ### Create first release
+
+* Show slide 54 and 55 (create release theory and script)
+* Now show the CLI
 
 ```bash
 ./demo-07-create-release-for-v3.sh 1 shipping first release to dev
@@ -56,6 +103,8 @@ Only run if you dont see the 3 endpoinst.
 ```
 * get result , say "1.899170756340027"
 * Explain this is the price in 100'000 , in this case ~`189k` USD.
+* write this down on the easy trix: https://docs.google.com/spreadsheets/d/17emmad7-UAUL298z-pYBWD76NWbFSmkphnpwSguZFTI/edit
+* color it yellow for v1 / green for v2. TODO: make auto coloring.
 
 ### Create v2 release
 
---
+++ deploy/custom-targets/vertex-ai/quickstart-v2-verify/demo-model-seems-ok.sh
@@ -0,0 +1,249 @@
+#! /bin/bash
+
+############################################################################################################
+# This is a copy of `model-seems-ok.sh`:
+#
+# * `model-seems-ok.sh`: do NOT touch (as its downloaded from the Cloud and uses CD values to work, see `skaffold.yaml`)
+# * `demo-model-seems-ok.sh`:  I can touch and play with locally.
+#
+# This is a local copy to wow the `astants` (bystanders) with model being called from local CURL
+############################################################################################################
+
+echo '⛔⛔ Note this is a dupe script for DemoVegas purposes. Do not spend too much time maintaining this. ⛔⛔'
+
+SCRIPT_VER="1.12local"
+INPUT_DATA_FILE="${1:-california-input-github.json}"
+TEMPORARY_BYPASS="false" # for local demo
+
+# This is for the local
+if [ -f _env_gaic.sh ]; then
+    source '_env_gaic.sh' # NO! This is executed from Github download - you cannot add it here!
+fi
+############################################################################################################
+# IMPORTANT TODOs: For the moment, the script
+# b/1: script is broken if you remove TEMPORARY_BYPASS => fix it (de to gcloud auth and gcloud missing!)
+# b/2: script uses a static model you should use `CLOUD_DEPLOY_customTarget_vertexAIEndpoint` instead
+# Sample CLOUD_DEPLOY_customTarget_vertexAIEndpoint="projects/rick-and-nardy-demo/locations/us-central1/endpoints/demo24-preprod"
+############################################################################################################
+
+############################################################################################################
+# 20240322 v1.12 Branched into Vegas "demo-model-seems-ok.sh" to test locally. Multiplied by 100k as per Ivan docs.
+# -> BRANCHED into Vegas "demo-model-seems-ok.sh"
+# 202403?? v1.11 somewhat I added back the source '_env_gaic.sh' which breaks the skaffold verify.
+# 20240322 v1.10 still problem. Removing it
+# 20240322 v1.9 removed the TEMP bypass. lets see.
+# 20240322 v1.8 Adding CLOUD_DEPLOY_customTarget_vertexAIEndpoint which finally allows me to customize the output to do the CURL to the proper ENDPOINT.
+# 20240322 v1.7 added a TEMPORARY_BYPASS
+# 20240322 v1.6 added AUTHORIZATION_BEARER depending if you have gcloud or not
+# 20240322 v1.5 fixed PN and gcloud
+# 20240321 v1.4 fixed for california and better error handling.
+# 20240321 v1.2 more env vars and adding version to the script, since this is invoked from GH and has different lifecycle :)
+############################################################################################################
+
+# Usage:
+# - ./model-seems-ok.sh (sensible defaults)
+# - MIN_VALUE=8000 MAX_VALUE=9000 ./model-seems-ok.sh (make your own values by setting these TWO env vars)
+#
+# California House price comes around 1..4.
+MIN_VALUE="${MIN_VALUE:-70000}"
+MAX_VALUE="${MAX_VALUE:-500000}"
+
+
+# this scripts calls the MODEL and checks if it works.
+echo "===================================================================================="
+echo "🚀 $0 v$SCRIPT_VER - start at $(date)"
+echo "🚀 Testing Verify - always returning TRUE unless ARGV[1] is 'FAIL'"
+#EMOJI="🚀"
+echo "Some sample CloudDeploy variables which might come handy:"
+echo "🌳 USER: $USER" # tree as ENVIRONMENT :)
+echo "🌳 TERM: $TERM" # tree as ENVIRONMENT :)
+echo "🌳 TERM_PROGRAM: $TERM_PROGRAM" # tree as ENVIRONMENT :)
+# These are documented in: https://cloud.google.com/deploy/docs/verify-deployment
+echo "🚀 TARGET_TYPE: $TARGET_TYPE"
+echo "🚀 CLOUD_DEPLOY_LOCATION: $CLOUD_DEPLOY_LOCATION"
+echo "🚀 CLOUD_DEPLOY_DELIVERY_PIPELINE: $CLOUD_DEPLOY_DELIVERY_PIPELINE"
+echo "🚀 CLOUD_DEPLOY_TARGET: $CLOUD_DEPLOY_TARGET"
+echo "🚀 CLOUD_DEPLOY_PROJECT: $CLOUD_DEPLOY_PROJECT"
+echo "🚀 CLOUD_DEPLOY_RELEASE: $CLOUD_DEPLOY_RELEASE"
+echo "🚀 CLOUD_DEPLOY_ROLLOUT: $CLOUD_DEPLOY_ROLLOUT"
+echo "🚀 CLOUD_DEPLOY_JOB_RUN: $CLOUD_DEPLOY_JOB_RUN"
+echo "🚀 CLOUD_DEPLOY_PHASE: $CLOUD_DEPLOY_PHASE"
+echo "CD Parameters now: see https://cloud.google.com/deploy/docs/parameters"
+# customTarget/vertexAIAliases
+echo "📊 customTarget:      $customTarget"
+#echo "📊 VERTEXAIALIASES:   $VERTEXAIALIASES"
+#echo "📊 VERTEX_AI_ALIASES: $VERTEX_AI_ALIASES"
+#echo "📊 VERTEXAI_ALIASES:  $VERTEXAI_ALIASES"
+echo "📊 CLOUD_DEPLOY_customTarget_vertexAIEndpoint: $CLOUD_DEPLOY_customTarget_vertexAIEndpoint"
+echo "📊📊 Vertex-grepping ENVs BEGIN"
+#env | grep -i vertex # echo "📊 vertexAIAliases: $vertexAIAliases"
+#printenv | grep -i vertex # echo "📊 vertexAIAliases: $vertexAIAliases"
+echo "📊📊  Vertex-grepping ENVs END"
+echo "===================================================================================="
+
+if [ "$TEMPORARY_BYPASS" = "true" ] ; then
+    echo "$0 ✅ [TEMPORARY_BYPASS=$TEMPORARY_BYPASS] Success v$SCRIPT_VER (I might need this to fill in all the targets, for example)"
+    exit 0
+fi
+
+if [ "${1:-nada}" = "FAIL" ] ; then
+    echo "$0 [QUICK_TEST] ⛔ FAILING"
+    exit 42
+#else
+#    echo "$0 [QUICK_TEST] ✅ SUCCESS (While I wait for Ivan's magic cURL)"
+#    exit 0
+fi
+
+## for Demo purporses pre Ivan fix:
+# 1. if dev2preprod -> always GOOD
+# 1. if preprod2prod -> I call the URL and I go further GOOD
+
+if [ "vertex-dev" = "$CLOUD_DEPLOY_TARGET" ] ; then
+    echo "$0 [QUICK_TEST] ✅ SUCCESS (While I wait for Ivan's magic cURL)"
+    exit 0
+else
+    echo "$0 [QUICK_TEST] CLOUD_DEPLOY_TARGET=$CLOUD_DEPLOY_TARGET is NOT dev: continuing.."
+fi
+
+# [alpine-wget] 🚀 CLOUD_DEPLOY_TARGET: vertex-dev
+
+set -euo pipefail
+
+# download ENV vars
+# Page: "https://github.com/palladius/clouddeploy-vertex-demo/blob/main/deploy/custom-targets/vertex-ai/quickstart-v2-verify/_env_gaic.sh"
+wget "https://raw.githubusercontent.com/palladius/clouddeploy-vertex-demo/main/deploy/custom-targets/vertex-ai/quickstart-v2-verify/_env_gaic.sh"   -O .github_env_vars.sh
+source '.github_env_vars.sh'
+echo  '.github_env_vars.sh sourced'
+
+if [ -f "$INPUT_DATA_FILE" ] ; then
+    echo "INPUT_DATA_FILE=$INPUT_DATA_FILE found: no action"
+else
+    echo "downloading INPUT_DATA_FILE from github.."
+    wget "https://raw.githubusercontent.com/palladius/clouddeploy-vertex-demo/main/deploy/custom-targets/vertex-ai/quickstart-v2-verify/california-input.json" -O "$INPUT_DATA_FILE"
+    cat "$INPUT_DATA_FILE"
+    # exit 43
+fi
+
+MODEL_ID="$DEMO_MODEL_ID"
+
+CORRECT_ENDPOINT_ID="${CORRECT_ENDPOINT_ID:-$DEV_ENDPOINT_ID}" # todo change by target
+
+set -euo pipefail
+
+echo "-------------"
+echo "ARGS ($#): '$*'"
+echo "ENV vars:"
+echo "🌱 DEV_ENDPOINT_ID : $DEV_ENDPOINT_ID (Using THIS)"
+echo "🌱 PROD_ENDPOINT_ID: $ENDPOINT_ID     "
+echo "🌱 CORRECT_ENDPOINT_ID: $CORRECT_ENDPOINT_ID  (Using DEV - todo should be depending on TARGET..)"
+echo "🌱 INPUT_DATA_FILE: $INPUT_DATA_FILE"
+echo "🌱 MODEL_NAME: $MODEL_NAME"
+echo "🌱 MODEL_ID: $MODEL_ID"
+echo "House value:"
+echo "🏡 MIN_VALUE: $MIN_VALUE"
+echo "🏡 MAX_VALUE: $MAX_VALUE"
+echo "-------------"
+# Specifying the explicit project id in case this came to but me in the future.
+############################################
+# REMOVEME - QUICK_TEST enabled of verify
+# ./model-seems-ok.sh        -> OK
+# ./model-seems-ok.sh FAIL   -> ERR
+# I only need this until Ivan provides me with a magic cURL for this model
+############################################
+# echo "Testing Verify - always returning TRUE unless ARGV[1] is 'FAIL'"
+# if [ "${1:-nada}" = "FAIL" ] ; then
+#     echo "$0 [QUICK_TEST] FAILING"
+#     exit 42
+# else
+#     echo "$0 [QUICK_TEST] SUCCESS (While I wait for Ivan's magic cURL)"
+#     exit 0
+# fi
+############################################
+# /removeme QUICK_TEST
+############################################
+
+echo "🧠 Testing model on project '$PROJECT_ID' on INPUT_DATA_FILE: $INPUT_DATA_FILE"
+echo "🧠 Verifying the house price is between $MIN_VALUE and $MAX_VALUE"
+
+
+#######################################################################
+# works but its slower - todo make it cleaner when everything works
+#######################################################################
+#echo "PROJECT_ID: $PROJECT_ID"
+#gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)"
+#export PROJECT_NUMBER2="$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")"
+PROJECT_NUMBER="$PROJECT_NUMBER2"
+echo "PROJECT_NUMBER: $PROJECT_NUMBER"
+#exit 42
+#######################################################################
+
+AUTHORIZATION_BEARER=""
+if which gcloud ; then
+  AUTHORIZATION_BEARER="Authorization: Bearer $(gcloud auth print-access-token)"
+else
+  AUTHORIZATION_BEARER=""
+fi
+
+#echo Note I removed gcloud auth so this will only work on CD
+#echo "AUTHORIZATION_BEARER: $AUTHORIZATION_BEARER"
+echo "Using Endpoint: $CORRECT_ENDPOINT_ID"
+
+curl \
+    -X POST \
+    -H "$AUTHORIZATION_BEARER" \
+    -H "Content-Type: application/json" \
+    https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_NUMBER}/locations/us-central1/endpoints/${CORRECT_ENDPOINT_ID}:predict \
+    -d "@${INPUT_DATA_FILE}" 2>/dev/null \
+       > output.json  # | tee output.json
+
+curl_ret="$?"
+echo "🕸️  cURL returned: '$curl_ret' (0 is good)"
+if [ "0" -eq "$curl_ret" ]; then
+    echo All good. cURL found the endpoint. Lets now see the values...
+
+else
+    echo cURL returned ERROR: "$curl_ret". Probably the endpoint wasnt found or is down.
+fi
+
+# Inspecting the output.json
+# Note that when it wont fail, probably this code needs fixing :)
+#if cat output.json | jq .error.code | egrep '^2' ; then
+if cat output.json | grep -q predictions; then
+    echo "✅ output.json seems to contain predictions: all good"
+else
+    ERR_CODE="$(cat output.json | jq .error.code)"
+    echo "- ❎ ERROR CODE: $ERR_CODE"
+    echo "- ❎ ERROR MESSAGE: $(cat output.json | jq .error.message)"
+    echo "❎ While the cURL returned correctly, the endpoint yielded an application-level error: Exiting."
+    echo "--"
+    cat output.json | jq .error
+    exit "$ERR_CODE"
+fi
+
+# this is in 100'000$ accordin to Docs.
+PREDICTED_HOUSE_PRICE_COEFFICIENT=$( cat output.json | jq .predictions[0] )
+HOUSE_PRICE=$(echo "scale=1; $PREDICTED_HOUSE_PRICE_COEFFICIENT * 100000 / 1" | bc)
+
+echo
+echo
+echo  "🏙️ Predicted 🇺🇸 🧸 California 🏡 house price: $HOUSE_PRICE💲"
+echo
+echo
+
+# num1=7.2
+# num2=7.5
+
+# awk -v n1=$num1 -v n2=$num2 'BEGIN {if (n1>n2) print n1" is greater than "n2; else print n1" is not greater than "n2}'
+
+
+# MIN_VALUE=1000.50
+# MAX_VALUE=5000.00
+# HOUSE_PRICE=2500.75
+
+if (( $(bc <<< "$HOUSE_PRICE >= $MIN_VALUE && $HOUSE_PRICE <= $MAX_VALUE") )); then
+    echo "[California🇺🇸🧸] 👍 HOUSE_PRICE=$HOUSE_PRICE is within range. Model seems good!"
+    exit 0
+else
+    echo "[California🇺🇸🧸] 👎 HOUSE_PRICE=$HOUSE_PRICE is outside range [$MIN_VALUE, $MAX_VALUE]"
+    exit 142
+fi
